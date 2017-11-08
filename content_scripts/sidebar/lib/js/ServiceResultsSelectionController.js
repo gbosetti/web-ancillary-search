aec961bdfbda7cca44b44236bfb1f0dfa57bdfda
Gabriela Bosetti <gabybosetti@gmail.com>
2017-11-08 11:47:05 -0300

    old results loading in both, recently installed extension and an 'already used' one

--- content_scripts/sidebar/lib/js/ServiceResultsSelectionController.js
+++ content_scripts/sidebar/lib/js/ServiceResultsSelectionController.js
@@ -4,21 +4,34 @@ serviceCreator.controller('ServiceResultsSelectionController', function($scope, 
 
     $scope.service = { 
       	results: {
-	        selector: undefined,
+	        selector: {
+	        	label: undefined,
+	        	value: undefined
+	        },
 	        preview: undefined  
 	    }
     };
-    $scope.availableSelectors;
 
     $scope.loadDataModel = function() {
       ServiceService.getService().then(function(service) {
-      	
       	$scope.service.results.selector = service.results.selector;
       	$scope.service.results.preview = service.results.preview;
+
+      	if($scope.service.results.selector){
+
+      		var availableSelectors = {};
+      		availableSelectors[service.results.selector.label] = [service.results.selector.value];
+
+      		$scope.onElementSelection({
+      			"previewSource": service.results.preview,
+      			"selectors": availableSelectors
+      		});
+      	}
       }); 
     };
     $scope.saveDataModel = function() {
     	//Splitted because there are other properties of "Results" managed by other controllers
+    	console.log("saving", $scope.service);
     	ServiceService.setResultsSelector($scope.service.results.selector);	
 		ServiceService.setResultsPreview($scope.service.results.preview);	
     };
@@ -27,44 +40,37 @@ serviceCreator.controller('ServiceResultsSelectionController', function($scope, 
 		$scope.disableDomElementSelection("tr, div:not(#andes-sidebar)");
     };
     $scope.loadSubformBehaviour = function() { 
-      $scope.enableDomElementSelection("tr, div:not(#andes-sidebar)", "onResultsContainerSelection", ".well");
+      $scope.enableDomElementSelection("tr, div:not(#andes-sidebar)", "onElementSelection", ".well");
     };
     $scope.onElementSelection = function(data){
-    	$scope.service.results.preview = data.previewSource;
-		$scope.showPreview(data.previewSource);
-		$scope.availableSelectors = data.selectors;
-	}
-	$scope.showPreview = function(previewSource){
-		document.querySelectorAll(".hidden").forEach(function(elem){
-			elem.classList.remove("hidden");
-		});
-		document.querySelector("#property-preview-image").src = previewSource;
-	}
-	$scope.onResultsContainerSelection = function(data){
 
-		$scope.showFormElement("#preview_group");
+    	$scope.showFormElement("#preview_group");
 		$scope.showFormElement("#selector_group");
-		
+
+    	$scope.service.results.preview = data.previewSource;
 		$scope.loadPreview("#result-preview-image", data.previewSource);
+
 		$scope.fillOccurrencesSelector(data.selectors);
 		document.querySelector("#result-selector").onchange();
-	};
+	}
 	$scope.fillOccurrencesSelector = function(selectors){
 
-		var selector = document.querySelector("#result-selector");
-			selector.innerHTML = "";
+		var select = document.querySelector("#result-selector");
+			select.innerHTML = "";
 
 		Object.keys(selectors).forEach(function(key) {
 
 			var elemsBySelectorLabel = key > 1? browser.i18n.getMessage("occurrences") : browser.i18n.getMessage("occurrence");
 			var opt = document.createElement("option");
+				opt.setAttribute("andes-occurrences", key);
 				opt.value = selectors[key][0];
 				opt.text = key + " " + elemsBySelectorLabel;
-			selector.add(opt); 
+			select.add(opt); 
 		});
 
-		selector.onchange = function(){
-			$scope.service.results.selector = this.value;
+		select.onchange = function(){
+
+			$scope.service.results.selector = { label: this.options[this.selectedIndex].getAttribute("andes-occurrences"), value: this.value};
 			browser.runtime.sendMessage({ 
 	    		"call": "selectMatchingElements",
 	    		"args": { "selector": this.value }
