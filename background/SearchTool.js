2178f21d5ee3ff1795834d19870ef08a0be396cd
Gabriela Bosetti <gabybosetti@gmail.com>
2018-03-16 16:32:49 -0300

    Extraction in invisible frame almost ready

--- background/SearchTool.js
+++ background/SearchTool.js
@@ -87,84 +87,9 @@ SearchTool.prototype.onVisualizationLoaded = function(tab){
 
   var me = this;
   return new Promise((resolve, reject) => {
-      me.getExternalContent(me.currentExecutionData).then(function(data){
-        resolve(data.service);
-      });
+        resolve(me.currentExecutionData.service);
   });
 }
-SearchTool.prototype.getParsedDocument = function(responseText){
-
-    return new window.DOMParser().parseFromString(responseText, "text/html");
-};
-SearchTool.prototype.getExternalContent = function(data) {
-  //TODO: move this behaviour to the searchTool class
-  var me = this;
-  return new Promise((resolve, reject) => {
-
-    const req = new window.XMLHttpRequest();
-        req.open('GET', data.service.url, false);
-        req.send(null);
-
-    var parsedDoc = me.getParsedDocument(req.responseText); //with def results. we need to trigger
-    var conceptDomElems = me.evaluateSelector(data.service.results.selector.value, parsedDoc);  
-    data.service.results = me.extractConcepts(conceptDomElems, data.service.results.properties);
-
-      resolve(data); 
-  });
-};
-SearchTool.prototype.evaluateSelector = function(selector, doc){
-  //TODO: acá se debería tener un strategy para laburar con diferentes tipos de selectores
-  return (new XPathInterpreter()).getElementsByXpath(selector, doc);
-};
-SearchTool.prototype.extractConcepts = function(domElements, propSpecs){
-  //TODO: Modularizar codigo
-  var concepts = [], me= this;
-  var keys = Object.keys(propSpecs);
-  
-  if(keys.length > 0){
-    keys.forEach(function(key){
-
-      var propElems = me.getMultiplePropsFromElements(propSpecs[key].relativeSelector, domElements);
-
-      for (i = 0; i < propElems.length; i++) { 
-        if (propElems[i] != null){ //If the object has the property, then
-
-          //console.log(propElems[i]);
-          if (concepts[i]){ //si hay concepto, se agrega propiedad
-            if(propElems[i] && propElems[i].textContent){
-              concepts[i][key] = propElems[i].textContent;
-            }else concepts[i][key] = propElems[i].src;
-          } //si no hay concepto, se crea
-          else{
-            concepts[i] = {};
-            if(propElems[i] && propElems[i].textContent){
-              concepts[i][key] = propElems[i].textContent;
-            }else concepts[i][key] = propElems[i].src;
-          }
-        } 
-
-        if(concepts[i][key] == undefined || concepts[i][key] == null) 
-          concepts[i][key] = " ";
-      }
-    });
-  } else alert("There are no properties defined. Results can not be extracted.");
-  return concepts;
-};
-SearchTool.prototype.getMultiplePropsFromElements = function(relativeSelector, relativeDomElems){
-  //TODO: acá se debería tener un strategy para laburar con diferentes tipos de selectores
-  var props = [], indexesOfInfoItems = Object.keys(relativeDomElems);
-
-  if(indexesOfInfoItems.length > 0){
-    indexesOfInfoItems.forEach(function(index){
-      var prop = (new XPathInterpreter()).getSingleElementByXpath(relativeSelector, relativeDomElems[index]);
-      
-      if(prop) {
-        props.push(prop);
-      } else props.push(" ");
-    });
-  }
-  return props; 
-};
 SearchTool.prototype.newDocumentWasLoaded = function(data) {
 
   var me = this;
--- content_scripts/visualizations/lib/js/dom-notification.js
+++ content_scripts/visualizations/lib/js/dom-notification.js
@@ -77,10 +77,75 @@ function ReadyToTrigger(){
 
 function ReadyToExtractResults(){
 	SearchStatus.call(this);
-	this.analyseDom = function(data){
+	this.analyseDom = function(data, caller){
 
 	  	console.log("READY TO EXTRACT!!!!");
-	}
+	  	var conceptDomElems = this.evaluateSelector(data.service.results.selector.value, document);  
+    	console.log(conceptDomElems);
+
+    	data.service.results = this.extractConcepts(conceptDomElems, data.service.results.properties);
+    	console.log(data.service.results);
+
+    	var me = this;
+		browser.runtime.sendMessage({ 
+			"call": "presentData",
+			"args": data
+		})
+	};
+	this.evaluateSelector = function(selector, doc){
+	  //TODO: acá se debería tener un strategy para laburar con diferentes tipos de selectores
+	  return (new XPathInterpreter()).getElementsByXpath(selector, doc);
+	};
+	this.getMultiplePropsFromElements = function(relativeSelector, relativeDomElems){
+	  //TODO: acá se debería tener un strategy para laburar con diferentes tipos de selectores
+	  var props = [], indexesOfInfoItems = Object.keys(relativeDomElems);
+
+	  if(indexesOfInfoItems.length > 0){
+	    indexesOfInfoItems.forEach(function(index){
+	      var prop = (new XPathInterpreter()).getSingleElementByXpath(relativeSelector, relativeDomElems[index]);
+	      
+	      if(prop) {
+	        props.push(prop);
+	      } else props.push(" ");
+	    });
+	  }
+	  return props; 
+	};
+	this.extractConcepts = function(domElements, propSpecs){
+	  //TODO: Modularizar codigo
+	  var concepts = [], me= this;
+	  var keys = Object.keys(propSpecs);
+	  var me = this;
+	  
+	  if(keys.length > 0){
+	    keys.forEach(function(key){
+
+	      var propElems = me.getMultiplePropsFromElements(propSpecs[key].relativeSelector, domElements);
+
+	      for (i = 0; i < propElems.length; i++) { 
+	        if (propElems[i] != null){ //If the object has the property, then
+
+	          //console.log(propElems[i]);
+	          if (concepts[i]){ //si hay concepto, se agrega propiedad
+	            if(propElems[i] && propElems[i].textContent){
+	              concepts[i][key] = propElems[i].textContent;
+	            }else concepts[i][key] = propElems[i].src;
+	          } //si no hay concepto, se crea
+	          else{
+	            concepts[i] = {};
+	            if(propElems[i] && propElems[i].textContent){
+	              concepts[i][key] = propElems[i].textContent;
+	            }else concepts[i][key] = propElems[i].src;
+	          }
+	        } 
+
+	        if(concepts[i][key] == undefined || concepts[i][key] == null) 
+	          concepts[i][key] = " ";
+	      }
+	    });
+	  } else alert("There are no properties defined. Results can not be extracted.");
+	  return concepts;
+	};
 }
 
 var searher = new Searcher();
