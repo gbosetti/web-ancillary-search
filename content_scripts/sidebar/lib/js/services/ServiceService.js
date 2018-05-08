b10d4c7b74b639d0f0ff07716e52e30a21c997f5
ulises-jeremias <ulisescf.24@gmail.com>
2018-05-08 15:46:56 -0300

    Added services dir

--- content_scripts/sidebar/lib/js/services/ServiceService.js
+++ content_scripts/sidebar/lib/js/services/ServiceService.js
@@ -1,282 +1,291 @@
 //TODO: implement a state for being sure getMatchingServices is retrieving something when everything is fully loaded
 
-function BuildingStrategy(){
-  this.uniqueNameService = function(name, client, deferred){};
-
+function BuildingStrategy() {
+  this.uniqueNameService = function(name, client, deferred) {};
 }
-function NewServiceEdition(){
+
+function NewServiceEdition() {
   BuildingStrategy.call(this);
 
-  this.uniqueNameService = function(name, client, deferred){
-
-      console.log("uniqueNameService  >  NewServiceEdition");
-      deferred.resolve(client.hasServiceNamed(name));
+  this.uniqueNameService = function(name, client, deferred) {
+    console.log("uniqueNameService  >  NewServiceEdition");
+    deferred.resolve(client.hasServiceNamed(name));
   }
 }
-function ExistingServiceEdition(){
+
+function ExistingServiceEdition() {
   BuildingStrategy.call(this);
 
-  this.uniqueNameService = function(name, client, deferred){
-
+  this.uniqueNameService = function(name, client, deferred) {
     console.log("uniqueNameService  >  ExistingServiceEdition");
-    if(client.services[client.currentServiceKey].name == name)
+    if (client.services[client.currentServiceKey].name == name) {
       deferred.resolve(false);
-    else deferred.resolve(client.hasServiceNamed(name));
+    } else {
+      deferred.resolve(client.hasServiceNamed(name));
+    }
   }
 }
 
+serviceCreator.service("ServiceService", [
+  "$q",
+  "$timeout",
+  function($q, $timeout) {
+    var $service = this;
 
-serviceCreator.service("ServiceService", ["$q", "$timeout", function($q, $timeout) {
+    this.services;
+    this.currentServiceKey;
+    this.buildingStrategy;
 
-  this.services;
-  this.currentServiceKey;
-  this.buildingStrategy;
-  var $service = this;
-
-  this.initialize = function(){
-
-    browser.storage.local.get("services").then((storedServices) => {
-
-        if(storedServices.services && Object.keys(storedServices.services).length > 0){
+    this.initialize = function() {
+      browser.storage.local.get("services").then((storedServices) => {
+        if (storedServices.services && Object.keys(storedServices.services).length > 0) {
           $service.services = storedServices.services;
-        }
-        else {
+        } else {
           $service.services = {}; //check if this is necessary. I thunk this is the def value, but just in case
         }
-    });
-  };
-  this.hasServiceNamed = function(name){
+      });
+    };
 
-    var serviceExists = false;
-    Object.keys($service.services).some(function(key, index) {
-      if ($service.services[key].name == name) {
-        serviceExists = true;
-        return;
-      }
-    });
-    return serviceExists;
-  };
-  this.getUrlDomain = function(url){
+    this.hasServiceNamed = function(name) {
+      var serviceExists = false;
+      Object.keys($service.services).some(function(key, index) {
+        if ($service.services[key].name == name) {
+          serviceExists = true;
+          return;
+        }
+      });
+      return serviceExists;
+    };
 
-    if(url){
-      var a = document.createElement('a');
+    this.getUrlDomain = function(url) {
+      if (url) {
+        var a = document.createElement('a');
         a.setAttribute('href', url);
-      return a.hostname;
+        return a.hostname;
+      }
+
+      return "*";
     }
-    return "*";
-  }
-  this.getMatchingServices = function(url){
+    this.getMatchingServices = function(url) {
 
-    var me=this, deferred = $q.defer();
-    $timeout(function() {
+      var me = this,
+        deferred = $q.defer();
+      $timeout(function() {
 
-      /*var matchingServices = {};
+        /*var matchingServices = {};
       Object.keys($service.services).forEach(function(i) {
         if(me.getUrlDomain(url) == me.getUrlDomain($service.services[i].url))
           matchingServices[$service.services[i].name] = $service.services[i];
       });
       deferred.resolve(matchingServices);*/
-      deferred.resolve($service.services);
+        deferred.resolve($service.services);
 
-    }, 500);
-    return deferred.promise;
-  };
-  this.newServiceWithName = function(name){
-    return {
-      name: name,
-      startingUrl: undefined,
-      url: "*",
-      input: {
-        selector:undefined,
-        preview: undefined
-      },
-      trigger: { /*TODO: this should be something similar to 'moreResults'*/
-          strategy: {
-            className: 'ClickBasedTrigger' /*and extra properties "by the strategy"*/
-          }
-      },
-      results: {
-        name: undefined,
-        selector:undefined,
-        preview: undefined,
-        properties:{} /*{name relativeSelector}*/
-      },
-      moreResults: {
-        className: 'NoRetrieval', /*and extra properties "by the strategy"*/
-      },
-      sorters: 'NoSorters', /*TODO: this should be something similar to 'moreResults'*/
-      filters: 'NoFilter' /*TODO: this should be something similar to 'moreResults'*/
+      }, 500);
+      return deferred.promise;
     };
-  };
-  this.asDeferred = function(action){
 
-    var deferred = $q.defer();
-    $timeout(function() {
+    this.newServiceWithName = function(name) {
+      return {
+        name: name,
+        startingUrl: undefined,
+        url: "*",
+        input: {
+          selector: undefined,
+          preview: undefined
+        },
+        trigger: {/* TODO: this should be something similar to 'moreResults' */
+          strategy: {
+            className: 'ClickBasedTrigger'/* and extra properties "by the strategy" */
+          }
+        },
+        results: {
+          name: undefined,
+          selector: undefined,
+          preview: undefined,
+          properties: {}/* {name relativeSelector} */
+        },
+        moreResults: {
+          className: 'NoRetrieval',
+          /* and extra properties "by the strategy" */
+        },
+        sorters: 'NoSorters',
+        /* TODO: this should be something similar to 'moreResults' */
+        filters: 'NoFilter'/* TODO: this should be something similar to 'moreResults' */
+      };
+    };
 
-      if(action == undefined) {
-        deferred.resolve();
-      }
-      else{
+    this.asDeferred = function(action) {
+      var deferred = $q.defer();
+      $timeout(function() {
+        if (action == undefined) {
+          deferred.resolve();
+        } else {
+          const returnElem = action();
+          deferred.resolve(returnElem);
+        }
+      }, 500);
+      return deferred.promise;
+    };
 
-        var returnElem = action();
-        deferred.resolve(returnElem);
-      }
+    this.logService = function() {
+      this.asDeferred(function() {
+        console.log($service.services[$service.currentServiceKey]);
+        return;
+      });
+    };
 
-    }, 500);
-    return deferred.promise;
-  };
-  this.logService = function() {
-    this.asDeferred(function(){
-      console.log($service.services[$service.currentServiceKey]);
-      return;
-    });
-  };
-  this.getService = function() { //Should be getCurrentService
+    this.getService = function() { //Should be getCurrentService
+      return this.asDeferred(function() {
+        return $service.services[$service.currentServiceKey];
+      });
+    };
 
-    return this.asDeferred(function(){
-      return $service.services[$service.currentServiceKey];
-    });
-  };
-  this.removeService = function(key){
+    this.removeService = function(key) {
+      return this.asDeferred(function() {
+        if ($service.services.hasOwnProperty(key)) {
+          delete $service.services[key];
+          $service.updateServices();
+        }
+        return;
+      });
+    };
 
-    return this.asDeferred(function(){
-      if ($service.services.hasOwnProperty(key)){
-        delete $service.services[key];
+    this.uniqueNameService = function(name) {
+      const deferred = $q.defer();
+      this.buildingStrategy.uniqueNameService(name, $service, deferred);
+
+      return deferred.promise;
+    };
+
+    this.setName = function(name) {
+      const self = this;
+
+      return this.asDeferred(function() {
+        if ($service.services[$service.currentServiceKey] == undefined) {
+          $service.services[$service.currentServiceKey] = self.newServiceWithName(name);
+        }
+
+        $service.services[$service.currentServiceKey].name = name;
+        return;
+      });
+    };
+
+    this.updateServices = function() {
+      browser.storage.local.set({"services": $service.services});
+    };
+
+    this.setInput = function(input) {
+      return this.asDeferred(function() {
+        $service.services[$service.currentServiceKey].input = input;
         $service.updateServices();
-      }
-      return;
-    });
-  };
-  this.uniqueNameService = function(name) {
+        return;
+      });
+    };
 
-    var deferred = $q.defer();
-    this.buildingStrategy.uniqueNameService(name, $service, deferred);
+    this.setUrl = function(url) {
+      return this.asDeferred(function() {
+        $service.services[$service.currentServiceKey].url = url;
+        return;
+      });
+    };
 
-    return deferred.promise;
-  };
-  this.setName = function(name) {
+    this.setTrigger = function(trigger) {
+      return this.asDeferred(function() {
+        $service.services[$service.currentServiceKey].trigger = trigger;
+        $service.updateServices();
+        return;
+      });
+    };
 
-    var me = this;
-    return this.asDeferred(function(){
+    this.setCurrentServiceKey = function(key) {
+      return this.asDeferred(function() {
+        $service.currentServiceKey = key;
+        return;
+      });
+    };
 
-      if($service.services[$service.currentServiceKey] == undefined)
-        $service.services[$service.currentServiceKey] = me.newServiceWithName(name);
+    this.setResultsName = function(name) {
+      return this.asDeferred(function() {
+        $service.services[$service.currentServiceKey].results.name = name;
+        return;
+      });
+    };
 
-      $service.services[$service.currentServiceKey].name = name;
-      return;
-    });
-  };
-  this.updateServices = function(){
-    browser.storage.local.set({ "services": $service.services });
-  };
-  this.setInput = function(input) {
+    this.setResultsSelector = function(selector) {
+      return this.asDeferred(function() {
+        $service.services[$service.currentServiceKey].results.selector = selector;
+        return;
+      });
+    };
 
-    return this.asDeferred(function(){
-      $service.services[$service.currentServiceKey].input = input;
-      $service.updateServices();
-      return;
-    });
-  };
-  this.setUrl = function(url) {
+    this.setResultsPreview = function(preview) {
+      return this.asDeferred(function() {
+        $service.services[$service.currentServiceKey].results.preview = preview;
+        return;
+      });
+    };
 
-    return this.asDeferred(function(){
-      $service.services[$service.currentServiceKey].url = url;
-      return;
-    });
-  };
-  this.setTrigger = function(trigger) {
+    this.setMoreResultsStrategy = function(className) {
+      return this.asDeferred(function() {
+        $service.services[$service.currentServiceKey].moreResults.className = className;
+        return;
+      });
+    };
 
-    return this.asDeferred(function(){
-      $service.services[$service.currentServiceKey].trigger = trigger;
-      $service.updateServices();
-      return;
-    });
-  };
-  this.setCurrentServiceKey = function(key) {
+    this.setMoreResultsExtraProps = function(props) {
 
-    return this.asDeferred(function(){
-      $service.currentServiceKey = key;
-      return;
-    });
-  };
-  this.setResultsName = function(name) {
+      return this.asDeferred(function() {
+        $service.services[$service.currentServiceKey].moreResults.props = props;
+        return;
+      });
+    };
 
-    return this.asDeferred(function(){
-      $service.services[$service.currentServiceKey].results.name = name;
-      return;
-    });
-  };
-  this.setResultsSelector = function(selector) {
+    this.updateServiceKey = function(oldKey, newKey) {
+      return this.asDeferred(function() {
+        $service.services[newKey] = $service.services[oldKey];
+        delete $service.services[oldKey];
+        $service.currentServiceKey = newKey;
+        return;
+      });
+    };
 
-    return this.asDeferred(function(){
-      $service.services[$service.currentServiceKey].results.selector = selector;
-      return;
-    });
-  };
-  this.setResultsPreview = function(preview) {
+    this.setBuildingStrategy = function(strategy) { // ExistingServiceEdition || NewServiceEdition
+      return this.asDeferred(function() {
+        $service.buildingStrategy = new window[strategy]();
+        return;
+      });
+    };
 
-    return this.asDeferred(function(){
-      $service.services[$service.currentServiceKey].results.preview = preview;
-      return;
-    });
-  };
-  this.setMoreResultsStrategy = function(className) {
+    this.getBuildingStrategy = function(strategy) { // TODO: remove
+      return this.asDeferred(function() {
+        return $service.buildingStrategy;
+      });
+    };
 
-    return this.asDeferred(function(){
-      $service.services[$service.currentServiceKey].moreResults.className = className;
-      return;
-    });
-  };
-  this.setMoreResultsExtraProps = function(props) {
+    this.setProperties = function(properties) { // ExistingServiceEdition || NewServiceEdition
+      //todo: add one by one, do not save an external collection
+      return this.asDeferred(function() {
+        $service.services[$service.currentServiceKey].properties = properties;
+        return;
+      });
+    };
 
-    return this.asDeferred(function(){
-      $service.services[$service.currentServiceKey].moreResults.props = props;
-      return;
-    });
-  };
-  this.updateServiceKey = function(oldKey, newKey) {
+    this.setSorters = function(sorters) {
+      //todo: add one by one, do not save an external collection
+      return this.asDeferred(function() {
+        $service.services[$service.currentServiceKey].sorters = sorters;
+        return;
+      });
+    };
 
-    return this.asDeferred(function(){
-      $service.services[newKey] = $service.services[oldKey];
-      delete $service.services[oldKey];
-      $service.currentServiceKey = newKey;
-      return;
-    });
-  };
-  this.setBuildingStrategy = function(strategy) { // ExistingServiceEdition || NewServiceEdition
+    this.setFilters = function(filters) {
+      //todo: add one by one, do not save an external collection
+      return this.asDeferred(function() {
+        $service.services[$service.currentServiceKey].filters = filters;
+        return;
+      });
+    };
 
-    return this.asDeferred(function(){
-      $service.buildingStrategy = new window[strategy]();
-      return;
-    });
-  };
-  this.getBuildingStrategy = function(strategy) { // TODO: remove
-
-    return this.asDeferred(function(){
-      return $service.buildingStrategy;
-    });
-  };
-  this.setProperties = function(properties) { // ExistingServiceEdition || NewServiceEdition
-    //todo: add one by one, do not save an external collection
-    return this.asDeferred(function(){
-      $service.services[$service.currentServiceKey].properties = properties;
-      return;
-    });
-  };
-  this.setSorters = function(sorters) {
-    //todo: add one by one, do not save an external collection
-    return this.asDeferred(function(){
-      $service.services[$service.currentServiceKey].sorters = sorters;
-      return;
-    });
-  };
-  this.setFilters = function(filters) {
-    //todo: add one by one, do not save an external collection
-    return this.asDeferred(function(){
-      $service.services[$service.currentServiceKey].filters = filters;
-      return;
-    });
-  };
-
-  this.initialize();
-}]);
+    this.initialize();
+  }
+]);
