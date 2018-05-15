3d9954c4167b6e41e81d3c91467c538f5ad5526e
ulises-jeremias <ulisescf.24@gmail.com>
2018-05-15 08:59:43 -0300

    Updated getMissingRequirementLocalizedId method

--- content_scripts/sidebar/lib/js/services/triggers/EnterBasedTrigger.js
+++ content_scripts/sidebar/lib/js/services/triggers/EnterBasedTrigger.js
@@ -2,29 +2,30 @@ function EnterBasedTrigger(client, data) {
   TriggerMechanism.call(this, client, data);
 
   this.loadParamsConfigControls = function() {
+    console.log("EnterBasedTrigger loadParamsConfigControls");
     console.log(client);
     client.addParamsConfigurationControls(document.createTextNode("EnterBasedTrigger"));
   }
 
   this.getProperties = function() {
+    console.log("EnterBasedTrigger getProperties");
     return {
-      className: this.constructor.name
+      className: this.constructor.name,
     };
   };
 
   this.loadSubformBehaviour = function() {
+    console.log("EnterBasedTrigger loadSubformBehaviour");
     client.showAllHiddenElements();
   };
 
-  this.getMissingRequirementLocalizedId = function() {
-    return "enter_on_trigger_error"
-  };
-
   this.areRequirementsMet = function() {
+    console.log("EnterBasedTrigger areRequirementsMet");
     return true;
   };
 
   this.onTriggerSelection = function(data) {
+    console.log("EnterBasedTrigger onTriggerSelection");
     client.showAllHiddenElements();
     this.removeErrorMessage();
   };
