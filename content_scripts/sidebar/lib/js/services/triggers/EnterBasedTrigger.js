6a02727d4074906e06817d06954e489c6ea92ee1
ulises-jeremias <ulisescf.24@gmail.com>
2018-05-15 17:10:27 -0300

    Updated triggers

--- content_scripts/sidebar/lib/js/services/triggers/EnterBasedTrigger.js
+++ content_scripts/sidebar/lib/js/services/triggers/EnterBasedTrigger.js
@@ -2,30 +2,24 @@ function EnterBasedTrigger(client, data) {
   TriggerMechanism.call(this, client, data);
 
   this.loadParamsConfigControls = function() {
-    console.log("EnterBasedTrigger loadParamsConfigControls");
-    console.log(client);
     client.addParamsConfigurationControls(document.createTextNode("EnterBasedTrigger"));
   }
 
   this.getProperties = function() {
-    console.log("EnterBasedTrigger getProperties");
     return {
       className: this.constructor.name,
     };
   };
 
   this.loadSubformBehaviour = function() {
-    console.log("EnterBasedTrigger loadSubformBehaviour");
     client.showAllHiddenElements();
   };
 
   this.areRequirementsMet = function() {
-    console.log("EnterBasedTrigger areRequirementsMet");
     return true;
   };
 
   this.onTriggerSelection = function(data) {
-    console.log("EnterBasedTrigger onTriggerSelection");
     client.showAllHiddenElements();
     this.removeErrorMessage();
   };
