533f551121ceb45dfd816692274338ff42cf27bc
SantiMaceri <santimaceri@gmail.com>
2018-06-15 11:57:12 -0300

    name fix main test

--- tests/unit/main.js
+++ tests/unit/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-describe("h1Manager.js", function() {
+describe("main.js", function() {
   describe("browserAction", function() {
     it("should register a listener for onClicked", function() {
       sinon.assert.calledOnce(browser.browserAction.onClicked.addListener);
