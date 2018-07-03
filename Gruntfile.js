68316d8a609a59bea8aa2d84abbf829a00f08ce1
ulises-jeremias <ulisescf.24@gmail.com>
2018-07-03 00:40:35 -0300

    Added gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function (grunt) {
+
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-compress");
+
+    const releaseRoot = "./build";
+
+    grunt.initConfig({
+        clean: {
+            build: ["build"],
+            webextension: ["build/web-ancillary-search"],
+            release: {
+                src: [releaseRoot],
+                options: {
+                    force: true
+                }
+            }
+        },
+        watch: {
+            files: ["Gruntfile.js", "src/background/**/*.js"],
+            tasks: "watchTask"
+        },
+        compress: {
+            webextension: {
+                options: {
+                    archive: "build/web-ancillary-search.zip"
+                },
+                files: [{
+                        expand: true,
+                        cwd: "build/web-ancillary-search",
+                        src: ["src/*"],
+                        dest: "build"
+                    }
+                ]
+            }
+        }
+    });
+
+    grunt.registerTask("watchTask", function () {
+            grunt.task.run("build");
+    });
+
+    grunt.registerTask("build:webextension", function () {
+        grunt.file.recurse("src", function(abspath, rootdir, subdir, filename) {
+            subdir = subdir ? subdir + "/" : "";
+            grunt.file.copy(abspath, "build/web-ancillary-search/" + subdir + filename);
+        });
+        grunt.task.run("compress:webextension");
+    });
+
+    grunt.registerTask("build", ["clean", "build:webextension"]);
+
+    grunt.registerTask("publish", ["clean:release", "build", "copy:release"]);
+
+    grunt.registerTask("default", ["build"]);
+};
