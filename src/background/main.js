de7a0f093877046b4c77e943fd48bcb38087c528
ulises-jeremias <ulisescf.24@gmail.com>
2018-07-03 00:40:28 -0300

    Updated background main

--- src/background/main.js
+++ src/background/main.js
@@ -2,12 +2,12 @@ var browserUI = new BrowserUiManager();
 
 browser.browserAction.onClicked.addListener(function updateIcon() {
 	console.log("cicking!")
-  	browserUI.toggleSidebar();
+  browserUI.toggleSidebar();
 });
 
 //From: sidebar to: addon, indicating it was sucessfully loaded
 browser.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  console.log("message: ", message.call, " (at main.js)");
+  console.log("message: ", message, " (at main.js)");
   if (browserUI[message.call]) {
     return browserUI[message.call](message.args); //in case you need to return a promise
   }
